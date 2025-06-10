f0725868d4f4d6beaaa0c9d003619cc3d5033caf
Ol1ver233 <106631225+PlsDrinkAqua@users.noreply.github.com>
2025-06-10 23:46:56 +0100

    Create my_stdint.h

---
+++ software/my_stdint.h
@@ -0,0 +1,15 @@
+#ifndef MY_STDINT_H
+#define MY_STDINT_H
+
+typedef unsigned char      uint8_t;
+typedef signed   char      int8_t;
+typedef unsigned short     uint16_t;
+typedef signed   short     int16_t;
+typedef unsigned int       uint32_t;
+typedef signed   int       int32_t;
+
+/* 新增：64 位整型 */
+typedef unsigned long long uint64_t;
+typedef signed   long long int64_t;
+
+#endif /* MY_STDINT_H */
