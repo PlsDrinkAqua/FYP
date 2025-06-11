64aa1db40af2ef2175c0856e739e178a40e5cd5c
Ol1ver233 <106631225+PlsDrinkAqua@users.noreply.github.com>
2025-06-11 01:09:53 +0100

    Update power_analysis.tcl

--- hardware/ASIC/Voltus/power_analysis.tcl
+++ hardware/ASIC/Voltus/power_analysis.tcl
@@ -1,9 +1,9 @@
-read_db "./"
+read_db "../Genus/"
 
 set_db power_method static
 set_db power_report_missing_nets true
 
 read_activity_file -reset
-read_activity_file -format TCF -scope cpu ./new_tcf.tcf
+read_activity_file -format TCF -scope cpu ../Xcelium/tcf.tcf
 
 report_power -rail_analysis_format VS -out_file ./power/rpt 
