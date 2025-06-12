9c31b6d8df06e1ef5a3181fa005e6ac2129a8cfc
Ol1ver233 <106631225+PlsDrinkAqua@users.noreply.github.com>
2025-06-13 00:06:24 +0100

    Create mmmc.tcl

---
+++ hardware/ASIC/Innovus/mmmc.tcl
@@ -0,0 +1,45 @@
+## library_sets
+create_library_set -name default_emulate_libset_max \
+    -timing { /home/ol521/mintNeuroCPU/library/gsclib045_svt_v4.7/gsclib045//timing/fast_vdd1v0_basicCells.lib }
+
+## opcond
+create_opcond -name default_emulate_opcond -process 1.0 -voltage 1.1 -temperature 0.0
+
+## timing_condition
+create_timing_condition -name default_emulate_timing_cond_max \
+    -opcond default_emulate_opcond \
+    -library_sets { default_emulate_libset_max }
+
+## rc_corner
+create_rc_corner -name default_emulate_rc_corner \
+    -temperature 0.0 \
+    -qrc_tech /home/ol521/mintNeuroCPU/library/gsclib045_svt_v4.7/gsclib045//qrc/qx/gpdk045.tch \
+    -pre_route_res 1.0 \
+    -pre_route_cap 1.0 \
+    -pre_route_clock_res 0.0 \
+    -pre_route_clock_cap 0.0 \
+    -post_route_res {1.0 1.0 1.0} \
+    -post_route_cap {1.0 1.0 1.0} \
+    -post_route_cross_cap {1.0 1.0 1.0} \
+    -post_route_clock_res {1.0 1.0 1.0} \
+    -post_route_clock_cap {1.0 1.0 1.0}
+
+## delay_corner
+create_delay_corner -name default_emulate_delay_corner \
+    -early_timing_condition { default_emulate_timing_cond_max } \
+    -late_timing_condition { default_emulate_timing_cond_max } \
+    -early_rc_corner default_emulate_rc_corner \
+    -late_rc_corner default_emulate_rc_corner
+
+## constraint_mode
+create_constraint_mode -name default_emulate_constraint_mode \
+    -sdc_files { /home/ol521/mintNeuroCPU/testing/picorv32-physical-design/interm/genus_to_innovus//picorv32.default_emulate_constraint_mode.sdc }
+
+## analysis_view
+create_analysis_view -name default_emulate_view \
+    -constraint_mode default_emulate_constraint_mode \
+    -delay_corner default_emulate_delay_corner
+
+## set_analysis_view
+set_analysis_view -setup { default_emulate_view } \
+                  -hold { default_emulate_view }
